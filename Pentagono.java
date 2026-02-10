85d98e635df561a24b8d1bbeb6a12cb02f14bad8
cynthia04-code <cynthia04@ciencias.unam.mx>
2026-02-10 11:05:15 -0600

    Create Pentagono.java

---
+++ Pentagono.java
@@ -0,0 +1,21 @@
+public class Pentagono extends PoligonoRegular {
+    
+    private static final int LADOS = 5;
+    private static final String TIPOFIGURA = "Pentagono";
+
+    public double PI = 3.1416;
+
+    public Pentagono(double longitudLado) {
+        super(LADOS, longitudLado, TIPOFIGURA);
+    }
+
+    @Override
+    public double calcularArea() {
+        /*Aquí va tu código*/
+    }
+
+    public double calcularApotema(){
+        /*Aquí va tu código*/
+    }
+
+}
