b9b5698c8efc29c9d0a8bbf8aafd639e6d645b86
cynthia04-code <cynthia04@ciencias.unam.mx>
2026-02-10 11:01:06 -0600

    Create Cuadrado class extending PoligonoRegular

---
+++ Cuadrado.java
@@ -0,0 +1,13 @@
+public class Cuadrado extends PoligonoRegular {
+    private static final int LADOS = 4;
+    private static final String TIPOFIGURA = "Cuadrado";
+
+    public Cuadrado(double longitudLado) {
+        super(LADOS, longitudLado, TIPOFIGURA);
+    }
+
+    @Override
+    public double calcularArea() {
+        /*Aquí va tu código*/
+    }
+}
